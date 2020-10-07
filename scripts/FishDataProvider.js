9bc6d6c8c7a619ebe471cc33488c18b9e23ccc1a
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-07 18:36:04 -0500

    added fish data javascript

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,40 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Bubba",
+        species: "",
+        length: ,
+        harvLocation: "",
+        food: "",
+        image:
+    },
+    {
+        name: "Scrappy",
+        species: "",
+        length: ,
+        harvLocation: "",
+        food: "",
+        image:
+    },
+    {
+        name: "Pepper",
+        species: "",
+        length: ,
+        harvLocation: "",
+        food: "",
+        image:
+    },
+    {
+        name: "Sammy",
+        species: "",
+        length: ,
+        harvLocation: "",
+        food: "",
+        image:""
+    },
+]
