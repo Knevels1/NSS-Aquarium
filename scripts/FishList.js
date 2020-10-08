695eb26d89f17086ba4696561a2f996f610ef46b
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-07 20:39:17 -0500

    Created FishList.js that appends header and container for holding list of fish to DOM

---
+++ scripts/FishList.js
@@ -0,0 +1,17 @@
+
+export const FishList = () => {
+
+
+const contentElement = document.querySelector(".contentContainer__left") 
+
+debugger
+
+contentElement.innerHTML += `
+<section class="fishList">
+<h2>Fish List</h2>
+<div class="fishContainer">
+ Placeholder for list of fish                       
+</div>
+</section>
+`
+}
