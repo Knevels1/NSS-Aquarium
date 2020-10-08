dec51c8cf4a223d43eea03a960d4604b86102daf
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-08 15:29:16 -0500

    added in the javascript info into sub dir. tips file name-> TipDataProvider.js

---
+++ scripts/tips/TipDataProvider.js
@@ -0,0 +1,14 @@
+const tipsForCare =[
+    {
+        name: "CARE TIPS",
+        topic: "Water Salination",
+        text: "no salty water",
+        text: "TEMPERATURE",
+        temp: "75-80 farenheit"
+
+    }
+]
+
+export const careTips = () => {
+    return tipsForCare.slice()
+}
