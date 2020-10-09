6d4439e20e814f8a54d67aaf19777763808f9be3
Knevels1 <kyle.a.nevels@icloud.com>
2020-10-08 20:06:08 -0500

    added in all location data into scripts/locations file name-> LocationDataProvider.js and did an export of the information

---
+++ scripts/locations/LocationDataProvider.js
@@ -0,0 +1,14 @@
+const locationData = [
+    {
+     quote: "We feel so big, until we gaze upon the stars and realize how small we truly are.",
+     author: "Kyle Nevels",
+     quote_location: "Mekong and Chao Phraya river basins in Thailand (formerly called Siam)",
+     locationImage: "./images/Mekong river.jpg",
+     locationName: "Mekong and Chao Phraya river basins",
+     located: "Thailand"
+    }
+]
+
+export const locationInfo = () => {
+    return locationData.slice()
+}
